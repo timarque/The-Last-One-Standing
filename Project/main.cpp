a4a0d3f2b6d795e531b92257aa9abab000c08e52
timarque <56795474+timarque@users.noreply.github.com>
2023-12-05 18:00:21 +0100

    First commit
    config cmake pour le projet
    et exo 2 avec adjusted paths etc

---
+++ Project/main.cpp
@@ -0,0 +1,316 @@
+#include<iostream>
+//for debug
+#include <fstream>
+
+//include glad before GLFW to avoid header conflict or define "#define GLFW_INCLUDE_NONE"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "camera.h"
+#include "shader.h"
+#include "object.h"
+
+#include <chrono>
+#include <thread>
+
+
+const int width = 1200;
+const int height = 1200;
+
+
+GLuint compileShader(std::string shaderCode, GLenum shaderType);
+GLuint compileProgram(GLuint vertexShader, GLuint fragmentShader);
+void processInput(GLFWwindow* window);
+
+
+
+#ifndef NDEBUG
+void APIENTRY glDebugOutput(GLenum source,
+	GLenum type,
+	unsigned int id,
+	GLenum severity,
+	GLsizei length,
+	const char* message,
+	const void* userParam)
+{
+	// ignore non-significant error/warning codes
+	if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+	std::cout << "---------------" << std::endl;
+	std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+	switch (source)
+	{
+	case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+	case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+	case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+	} std::cout << std::endl;
+
+	switch (type)
+	{
+	case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+	case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+	case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+	case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+	case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+	case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+	case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+	} std::cout << std::endl;
+
+	switch (severity)
+	{
+	case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+	case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+	case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+	case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+	} std::cout << std::endl;
+	std::cout << std::endl;
+}
+#endif
+
+Camera camera(glm::vec3(0.0, 0.0, 0.1));
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Welcome to exercice 14: " << std::endl;
+	std::cout << "Refactoring\n"
+		"At this point your code should start to be ugly.\n"
+		"It's time to do a good refactoring of your code from ex 13.\n"
+		"Make classes for your shader and camera. \n";
+
+	//Boilerplate
+	//Create the OpenGL context 
+	if (!glfwInit()) {
+		throw std::runtime_error("Failed to initialise GLFW \n");
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifndef NDEBUG
+	//create a debug context to help with Debugging
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+#endif
+
+
+	//Create the window
+	GLFWwindow* window = glfwCreateWindow(width, height, "Project", nullptr, nullptr);
+	if (window == NULL)
+	{
+		glfwTerminate();
+		throw std::runtime_error("Failed to create GLFW window\n");
+	}
+
+	glfwMakeContextCurrent(window);
+	//load openGL function
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		throw std::runtime_error("Failed to initialize GLAD");
+	}
+
+#ifndef NDEBUG
+	int flags;
+	glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+	if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+	{
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback(glDebugOutput, nullptr);
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+	}
+#endif
+
+	const std::string sourceV = "#version 330 core\n"
+		"in vec3 position; \n"
+		"in vec2 texcoord; \n"
+		"out vec2 v_tex; \n"
+
+		"uniform mat4 M; \n"
+		"uniform mat4 V; \n"
+		"uniform mat4 P; \n"
+		" void main(){ \n"
+		"gl_Position = P*M*V*vec4(position, 1);\n"
+		"v_tex = texcoord; \n"
+
+		"}\n";
+	const std::string sourceF = "#version 330 core\n"
+		"out vec4 FragColor;"
+		"precision mediump float; \n"
+		"in vec2 v_tex; \n"
+		"uniform sampler2D ourTexture; \n"
+		"void main() { \n"
+		"FragColor = texture(ourTexture, v_tex); \n"
+		"} \n";
+
+
+	char fileVert[128] = "../../../../Project/vertSrc.txt";
+	char fileFrag[128] = "../../../../Project/fragSrc.txt";
+	Shader shader(fileVert, fileFrag);
+
+	const float positionsData[15] = {
+		// vertices		  // texture coords
+		-1.0, -1.0, 0.0,	0.0, 0.0,
+		 1.0, -1.0, 0.0,	1.0, 0.0,
+		 0.0,  1.0, 0.0,	0.5, 1.0,
+	};
+
+
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+
+	stbi_set_flip_vertically_on_load(true);
+	int imWidth, imHeight, imNrChannels;
+	char file[128] = "../../../../Project/container.jpg";
+	unsigned char* data = stbi_load(file, &imWidth, &imHeight, &imNrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imWidth, imHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Failed to Load texture" << std::endl;
+		const char* reason = stbi_failure_reason();
+		std::cout << reason << std::endl;
+	}
+
+	stbi_image_free(data);
+
+	//Create the buffer
+	GLuint VBO, VAO;
+	//generate the buffer and the vertex array
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	//define VBO and VAO as active buffer and active vertex array
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positionsData), positionsData, GL_STATIC_DRAW);
+
+	auto attribute = glGetAttribLocation(shader.ID, "position");
+	glEnableVertexAttribArray(attribute);
+	glVertexAttribPointer(attribute, 3, GL_FLOAT, false, 5 * sizeof(float), (void*)0);
+
+	auto att_tex = glGetAttribLocation(shader.ID, "texcoord");
+	glEnableVertexAttribArray(att_tex);
+	glVertexAttribPointer(att_tex, 2, GL_FLOAT, false, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+
+	//desactive the buffer
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
+	double prev = 0;
+	int deltaFrame = 0;
+	//fps function
+	auto fps = [&](double now) {
+		double deltaTime = now - prev;
+		deltaFrame++;
+		if (deltaTime > 0.5) {
+			prev = now;
+			const double fpsCount = (double)deltaFrame / deltaTime;
+			deltaFrame = 0;
+			std::cout << "\r FPS: " << fpsCount;
+			std::cout.flush();
+		}
+		};
+
+
+	glm::mat4 model = glm::mat4(1.0);
+	model = glm::translate(model, glm::vec3(0.5, 0.5, -1.0));
+	model = glm::scale(model, glm::vec3(0.5, 0.5, 1.0));
+
+
+	glm::mat4 view = camera.GetViewMatrix();
+	glm::mat4 perspective = glm::perspective(45.0, 500.0 / 500.0, 0.01, 100.0);
+
+	//sync with the screen refresh rate
+	glfwSwapInterval(1);
+	//Rendering
+
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+		view = camera.GetViewMatrix();
+		glfwPollEvents();
+		double now = glfwGetTime();
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glBindVertexArray(VAO);
+
+		//2. Use the shader Class to send the uniform
+		shader.use();
+
+		shader.setMatrix4("M", model);
+		shader.setMatrix4("V", view);
+		shader.setMatrix4("P", perspective);
+
+		shader.setInteger("ourTexture", 0);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		fps(now);
+		glfwSwapBuffers(window);
+	}
+
+	//clean up ressource
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
+
+
+
+void processInput(GLFWwindow* window) {
+	//3. Use the cameras class to change the parameters of the camera
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboardMovement(LEFT, 0.1);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboardMovement(RIGHT, 0.1);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboardMovement(FORWARD, 0.1);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboardMovement(BACKWARD, 0.1);
+
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		camera.ProcessKeyboardRotation(1, 0.0, 1);
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		camera.ProcessKeyboardRotation(-1, 0.0, 1);
+
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+		camera.ProcessKeyboardRotation(0.0, 1.0, 1);
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		camera.ProcessKeyboardRotation(0.0, -1.0, 1);
+
+
+}
